e1cc0d3312e23752f53a264ca00b0eb3c71abc91
Andrew <13761224431@163.com>
2014-08-15 17:04:14 +0800

    Final

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,18 +3,14 @@
 
 angular.module('yo3App')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-        "Item 1",
-        "Item 2",
-        "Item 3"
-    ];
+    $scope.todos = [];
 
     $scope.addTodo = function() {
         $scope.todos.push($scope.todo);
         $scope.todo = '';
-    }
+    };
 
     $scope.removeTodo = function (index) {
         $scope.todos.splice(index,1);
-    }
+    };
   });
